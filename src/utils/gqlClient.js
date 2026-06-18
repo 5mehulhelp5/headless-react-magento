256266959756ece3ae1fa53312353688c66f4221
Jaimin Patel <jaimin30j@gmail.com>
2026-06-18 16:06:18 +0400

    Change GraphQL client URL to local API endpoint
    Updated GraphQL client to use a local API endpoint.

--- src/utils/gqlClient.js
+++ src/utils/gqlClient.js
@@ -1,7 +1,9 @@
 import { GraphQLClient } from 'graphql-request'
 
-const gqlClient = new GraphQLClient(
-    import.meta.env.VITE_GRAPHQL_URL
-)
+// const gqlClient = new GraphQLClient(
+//     import.meta.env.VITE_GRAPHQL_URL
+// )
+
+const gqlClient = new GraphQLClient('/api/graphql');
 
 export default gqlClient
